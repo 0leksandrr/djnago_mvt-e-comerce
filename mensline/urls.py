b7c0eebade54acbd15fb59e06c633adae1fc0d1a
0leksandrr <tsybulskyy.sasha@gmail.com>
2024-12-30 21:07:22 +0200

    Add logo

--- mensline/urls.py
+++ mensline/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'store'
+
 urlpatterns = [
     path('securelogin/', admin.site.urls),
     path('', views.home, name='home'),
